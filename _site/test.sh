d940c7bea0f7c531e76ebade575546362508107c
fanventory <190260924@qq.com>
2023-07-26 20:08:57 +0800

    update

---
+++ _site/test.sh
@@ -0,0 +1,12 @@
+
+cd _site
+echo "============>cd _site"
+
+git add .
+echo "============>git add"
+
+git commit -m "update"
+echo "============>git commit"
+
+git push -u origin "master"
+echo "============>git push"
