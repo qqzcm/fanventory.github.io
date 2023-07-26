4c2cd37d2e5a7b2f0b72866da7781381e65f1d67
fanventory <190260924@qq.com>
2023-07-26 20:17:16 +0800

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 jekyll build --incremental
 echo "============>jekyll build"
 
-<<<<<<< HEAD
-mv /e/笔记/image /e/笔记/myblog/_site/
-=======
 cp -r /e/笔记/image /e/笔记/myblog/_site/
->>>>>>> 8913020 (update)
 echo "============>xcopy"
 
 cd _site
